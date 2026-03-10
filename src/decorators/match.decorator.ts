1bc2bad03d2c8b67197f690079380e22f1a1985d
nahuel <nahuelmellusso@gmail.com>
2026-03-10 13:01:33 -0300

    update decorator

--- src/decorators/match.decorator.ts
+++ src/decorators/match.decorator.ts
@@ -1,9 +1,9 @@
-import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+import { registerDecorator, ValidationArguments, ValidationOptions } from "class-validator";
 
 export function Match(property: string, validationOptions?: ValidationOptions) {
   return (object: any, propertyName: string) => {
     registerDecorator({
-      name: 'match',
+      name: "match",
       target: object.constructor,
       propertyName: propertyName,
       options: validationOptions,
